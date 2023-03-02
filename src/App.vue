f03d39fcb2e01496318c6f39507bfcd400da6493
‘CptKay’ <sammerz@bluewin.ch>
2023-03-02 08:51:20 +0100

    init

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+
+  <h1>Welcome to Opportunity</h1>
+   <h2>Date</h2>
+
+<!-- footer -->
 </template>
 
 <script>
@@ -8,6 +13,13 @@ import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      title: "Welcome to Opportunity",
+      currentDate:"Date",
+      entries: [],
+    };
+  },
   components: {
     HelloWorld
   }
@@ -15,12 +27,48 @@ export default {
 </script>
 
 <style>
+ @import url('https://fonts.googleapis.com/css2?family=Inter:wght@500;900&display=swap%22');
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Inter, Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  /* color: #0F05A0; */
   margin-top: 60px;
+  background: #E8EFF4;
 }
+
+#h1 {
+
+}
+
+#h2 {
+  color: #9AA7B1
+  
+}
+
+ul.no-bullets {
+  list-style-type: none; /* Remove bullets */
+  padding: 0; /* Remove padding */
+  margin: 0; /* Remove margins */
+}
+
+#rectangle {
+  width: 960px;
+  height: 180px;
+  background: #0F05A0;
+
+}
+
+#time {
+  color: #EB5E00
+}
+
+#text {
+  color: #FFBFAB
+
+}
+
+
 </style>
